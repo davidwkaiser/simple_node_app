c56c71ef3a71819443b3df61c74fb8a3571781d7
davidwkaiser <davidwkaiser@yahoo.com>
2017-03-26 19:22:12 -0500

    get message passing to work

--- server.js
+++ server.js
@@ -3,18 +3,18 @@ var express = require('express');
 var app = express();
 var ig = require('instagram-node').instagram();
 ig.use({
-  access_token: MY_ACCESS_TOKEN
+  access_token: 'MY_ACCESS_TOKEN'
 })
 
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 app.get('/', function(req, res){
-  console.log("get!")
-  res.render('pages/index.ejs');
+  // console.log("get!")
+  res.render('pages/index', {m: 'i am data'});
 });
 
 
 //Start the server on port 8080
 app.listen(8080, function () {
-  console.log("express has started on port 8080");
+  // console.log("express has started on port 8080");
 });
