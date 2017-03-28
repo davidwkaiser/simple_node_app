bee0ea80ebf5453990043ea809c0ef33180b0961
davidwkaiser <davidwkaiser@yahoo.com>
2017-03-27 21:45:30 -0500

    add IG logic to server

--- server.js
+++ server.js
@@ -1,20 +1,47 @@
-// grab express
+// GRAB THE PACKAGES/VARIABLES WE NEED
+// ==================================================
 var express = require('express');
-var app = express();
-var ig = require('instagram-node').instagram();
-ig.use({
-  access_token: 'MY_ACCESS_TOKEN'
-})
+var app     = express();
+var ig      = require('instagram-node').instagram();
 
+// CONFIGURE THE APP
+// ==================================================
+// tell node where to look for site resources
 app.use(express.static(__dirname + '/public'));
+
+// set the view engine to ejs
 app.set('view engine', 'ejs');
-app.get('/', function(req, res){
-  // console.log("get!")
-  res.render('pages/index', {m: 'i am data'});
+
+// configure instagram app with client_id, client_secret, and access_token
+ig.use({
+  // get access token here: http://instagram.pixelunion.net/
+  access_token: 'MY_ACCESS_TOKEN',
 });
 
+// alternatively we can use the client_id and client_secret
+// for now we'll use the access_token way
+// ig.use({
+  // get these from when we create our app as an instagram developer
+  // https://www.instagram.com/developer/
+  // client_id: 'MY_CLIENT_ID',
+  // client_secret: 'MY_CLIENT_SECRET'
+// });
 
-//Start the server on port 8080
-app.listen(8080, function () {
-  // console.log("express has started on port 8080");
+// SET THE ROUTES
+// ===================================================
+// home page route - popular images
+app.get('/', function(req, res) {
+
+    // use the instagram package to get popular media
+    ig.user_self_media_recent(function(err, medias, pagination, remaining, limit) {
+        // render the home page and pass in the popular images
+
+        res.render('pages/index', { grams: medias });
+    });
+
 });
+
+// START THE SERVER
+// ==================================================
+app.listen(8080);
+console.log('App started! Look at http://localhost:8080');
